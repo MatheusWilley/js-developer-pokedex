6df881ecb1894db060469ecfca356eb98e08aa58
Matheus <matheuswilley2757@gmail.com>
2024-11-16 19:59:19 -0300

    Tornando o código mais limpo e fácil de entender, simplificando a criação da instância do Pokémon, fazendo tratamento de erros e mapeando o tipo de Pokémon simplificado

---
+++ src/js/poke-api.js
@@ -0,0 +1,44 @@
+const pokeApi = {};
+
+// Converte os detalhes do Pokémon para a instância da classe Pokemon
+function convertPokeApiDetailToPokemon(pokeDetail){
+    const pokemon = new Pokemon(
+        pokeDetail.id,
+        pokeDetail.name,
+        pokeDetail.types.map((typeSlot) => typeSlot.type.name),
+        pokeDetail.sprites.other.dream_world.front_default
+    );
+
+    // Atribui o primeiro tipo como o principal
+    pokemon.type = pokemon.types[0];
+    return pokemon;
+}
+
+// Busca os detalhes de um Pokémon
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
+        .catch((error) => {
+            console.error('Erro ao obter detalhes do Pokémon:', error);
+        });
+};
+
+// Busca a lista de Pokémons
+pokeApi.getPokemons = async (offset = 0, limit = 5) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+    try{
+        const response = await fetch(url);
+        const jsonBody = await response.json();
+        const pokemons = jsonBody.results;
+
+        const pokemonsDetailsPromises = pokemons.map(pokeApi.getPokemonDetail);
+        const pokemonDetails = await Promise.all(pokemonsDetailsPromises);
+
+        return pokemonDetails;
+    } catch (error) {
+        console.error('Erro ao carregar Pokémons:', error);
+        return [];
+    }
+};
